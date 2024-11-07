a6110f8dc9c936993caa74493172d3966db0456e
Mac <maclong9@icloud.com>
2024-11-07 00:38:13 +0000

    feat(Auth): signup/login functioning

--- Sources/App/Controllers/WebController.swift
+++ Sources/App/Controllers/WebController.swift
@@ -32,7 +32,7 @@ struct WebController {
     
     /// Add routes for webpages
     func addRoutes(to router: Router<Context>) {
-        /// Unauthenticated routes
+        // Unauthenticated routes
         router.group()
             .get("/login", use: self.login)
             .post("/login", use: self.loginDetails)
@@ -40,7 +40,7 @@ struct WebController {
             .post("/signup", use: self.signupDetails)
             .get("/", use: self.home)
         
-        /// Authenticated routes
+        // Authenticated routes
         router.group()
             .add(middleware: self.sessionAuthenticator)
             .add(middleware: RedirectMiddleware(to: "/login"))
@@ -60,20 +60,47 @@ struct WebController {
         )
     }
     
-    /// Dashboard page listing todos and with add todo UI
-    @Sendable func dashboard(request: Request, context: Context) async throws -> HTML {
-        // get user and list of todos attached to user from database
-        let user = try context.requireIdentity()
-        let todos = try await user.$todos.get(on: self.fluent.db())
-        let object: [String: Any] = [
-            "name": user.name,
-            "todos": todos,
-        ]
-        
-        return HTML(
-            title: "Dashboard",
-            content: DashboardView(name: "placeholder").render()
-        )
+    struct SignupDetails: Decodable {
+        let name: String
+        let email: String
+        let password: String
+        let confirmPassword: String
+    }
+    
+    /// Signup page
+    @Sendable func signup(request: Request, context: Context) async throws -> HTML {
+        HTML(title: "Sign Up", content: AuthView().render())
+    }
+    
+    /// Signup POST page
+    @Sendable func signupDetails(request: Request, context: Context) async throws -> Response {
+        let details = try await request.decode(as: SignupDetails.self, context: context)
+        do {
+            if details.password != details.confirmPassword {
+                throw HTTPError(.conflict, message: "Passwords do not match")
+            }
+            
+            let user = try await User.create(
+                name: details.name,
+                email: details.email,
+                password: details.password,
+                db: self.fluent.db()
+            )
+            // After successful signup, create session and log user in
+            try context.sessions.setSession(user.requireID())
+            return .redirect(to: "/dashboard", type: .found)
+        } catch let error as HTTPError {
+            if error.status == .conflict {
+                var response = try HTML(
+                    title: "Sign Up",
+                    description: "Take control o f your life with this wonderful todo list application.",
+                    content: AuthView(errorMessage: "Email already in use.").render()
+                ).response(from: request, context: context)
+                response.status = .badRequest
+                return response
+            }
+            throw error
+        }
     }
     
     /// Login page
@@ -130,41 +157,19 @@ struct WebController {
         }
     }
     
-    struct SignupDetails: Decodable {
-        let name: String
-        let email: String
-        let password: String
-    }
-    
-    /// Signup page
-    @Sendable func signup(request: Request, context: Context) async throws -> HTML {
-        HTML(title: "Sign Up", content: AuthView().render())
-    }
-    
-    /// Signup POST page
-    @Sendable func signupDetails(request: Request, context: Context) async throws -> Response {
-        let details = try await request.decode(as: SignupDetails.self, context: context)
-        do {
-            let user = try await User.create(
-                name: details.name,
-                email: details.email,
-                password: details.password,
-                db: self.fluent.db()
-            )
-            // After successful signup, create session and log user in
-            try context.sessions.setSession(user.requireID())
-            return .redirect(to: "/dashboard", type: .found)
-        } catch let error as HTTPError {
-            if error.status == .conflict {
-                var response = try HTML(
-                    title: "Sign Up",
-                    description: "Take control o f your life with this wonderful todo list application.",
-                    content: AuthView(errorMessage: "Email already in use.").render()
-                ).response(from: request, context: context)
-                response.status = .badRequest
-                return response
-            }
-            throw error
-        }
+    /// Renders the dashboard page that provides CRUD operations for managing todos
+    @Sendable func dashboard(request: Request, context: Context) async throws -> HTML {
+        // get user and list of todos attached to user from database
+        let user = try context.requireIdentity()
+        let todos = try await user.$todos.get(on: self.fluent.db())
+        let object: [String: Any] = [
+            "name": user.name,
+            "todos": todos,
+        ]
+        
+        return HTML(
+            title: "Dashboard",
+            content: DashboardView(name: "placeholder").render()
+        )
     }
 }
