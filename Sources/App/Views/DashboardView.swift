c8d9dab1e947cc111c5dccd2f670ea3bf5bef76a
Mac <maclong9@icloud.com>
2024-11-08 23:51:25 +0000

    refactor(DashboardView, HomeView): componentise

--- Sources/App/Views/DashboardView.swift
+++ Sources/App/Views/DashboardView.swift
@@ -1,44 +1,70 @@
 struct DashboardView {
     let user: User
     let todos: [Todo]
+    let error: String?
+    
+    init(user: User, todos: [Todo], error: String? = nil) {
+        self.user = user
+        self.todos = todos
+        self.error = error
+    }
+    
+    func header() -> String {
+        """
+        <header>
+            <h1>Welcome Back, \(user.name)</h1>
+            <button class="unstyle" onclick="settings.showModal()">⚙️</button>
+            <dialog id="settings">
+                \(AuthView(action: .updateProfile).render())
+            </dialog>
+            <dialog id="deletion" class="surface">
+                <h2>Are you sure?</h2>
+                <p>
+                   This action is final and cannot be reversed. <br />
+                   You will not be able to recover your account later.
+                </p>
+                <form class="btn-group" action="delete-account">
+                    <button type="button" onclick="deletion.close()">Cancel</button>
+                    <button class="destructive" type="submit">Delete Account</button>
+                </form>
+            </dialog>
+        </header>
+        """
+    }
+    
+    func form() -> String {
+        """
+        <form class="todo-form" action="/api/todos" method="post">
+            <div class="form-group">
+                <label for="title">Add a Todo</label>
+                <div class="inputs">
+                    <input 
+                        type="text" 
+                        id="title" 
+                        name="title"
+                        placeholder="What do you want to achieve?"
+                        required
+                    >
+                    <input class="btn" type="submit" value="+" />
+                </div>
+            </div>
+        </form>        
+        """
+    }
+    
+    func list() -> String {
+        """
+            <p>No todo items yet...</p>
+        """
+    }
     
     func render() -> String {
-        return """
+        """
         <section class="dashboard">
-            <header>
-                <h1>Welcome Back, \(user.name)</h1>
-                <button class="unstyle" onclick="settings.showModal()">⚙️</button>
-                <dialog id="settings">
-                    \(AuthView(action: .updateProfile).render())
-                </dialog>
-                <dialog id="deletion" class="surface">
-                    <h2>Are you sure?</h2>
-                    <p>
-                       This action is final and cannot be reversed. <br />
-                       You will not be able to recover your account later.
-                    </p>
-                    <form class="btn-group" action="delete-account">
-                        <button type="button" onclick="deletion.close()">Cancel</button>
-                        <button class="destructive" type="submit">Delete Account</button>
-                    </form>
-                </dialog>
-            </header>
+            \(header())
             <main>
-                <form class="todo-form" action="/api/todos" method="post">
-                    <div class="form-group">
-                        <label for="email">Add a Todo</label>
-                        <div class="inputs">
-                            <input 
-                                type="text" 
-                                id="new-todo" 
-                                name="new-todo"
-                                placeholder="What do you want to achieve?"
-                                required
-                            >
-                            <input class="btn" type="submit" value="+" />
-                        </div>
-                    </div>
-                </form>
+                \(form())
+                \(list())
             </main>
         </section>
         """
--- Sources/App/Views/HomeView.swift
+++ Sources/App/Views/HomeView.swift
@@ -6,63 +6,63 @@ struct HomeView {
     }
     
     func heroSection() -> String {
-    """
-    <section class="grid">
-      <div class="text">
-        <h1>
-          Take Control of
-          <span class="gradient-highlight">Your Life</span>
-        </h1>
-        <p>
-          With this wonderful application, designed to make your life easier while
-          staying out of the way. Take the first step in your new journey.
-        </p>
-        \(ActionButtons(isLoggedIn: isLoggedIn).render())
-      </div>
-      <img src="images/hero.svg" />
-    </section>
-    """
+        """
+        <section class="grid">
+          <div class="text">
+            <h1>
+              Take Control of
+              <span class="gradient-highlight">Your Life</span>
+            </h1>
+            <p>
+              With this wonderful application, designed to make your life easier while
+              staying out of the way. Take the first step in your new journey.
+            </p>
+            \(ActionButtons(isLoggedIn: isLoggedIn).render())
+          </div>
+          <img src="images/hero.svg" />
+        </section>
+        """
     }
     
     func featureList() -> String {
-    """
-    <section class="grid gap three">
-        <div class="feature">
-            <span class="icon">🎯</span>
-            <h2>Reach Your Goals Faster</h2>
-            <p>With quick, simple and easy task management.</p>
-        </div>
-        <div class="feature">
-            <span class="icon">📲</span>
-            <h2>Check Off On the Go</h2>
-            <p>Check off tasks as you rush about your busy day.</p>
-        </div>  
-        <div class="feature">
-            <span class="icon">😎</span>
-            <h2>Chill Out and Relax</h2>
-            <p>With your life back on track, enjoy more time relaxing.</p>
-        </div>
-    </section>
-    """
+        """
+        <section class="grid gap three">
+            <div class="feature">
+                <span class="icon">🎯</span>
+                <h2>Reach Your Goals Faster</h2>
+                <p>With quick, simple and easy task management.</p>
+            </div>
+            <div class="feature">
+                <span class="icon">📲</span>
+                <h2>Check Off On the Go</h2>
+                <p>Check off tasks as you rush about your busy day.</p>
+            </div>  
+            <div class="feature">
+                <span class="icon">😎</span>
+                <h2>Chill Out and Relax</h2>
+                <p>With your life back on track, enjoy more time relaxing.</p>
+            </div>
+        </section>
+        """
     }
     
     func callToAction() -> String {
-    """
-    <section>
-        <div class="cta">
-            <h2 class="gradient-highlight">Get Started Now</h1>
-            <p>Sign up and get your life back on track! Stay organized, boost productivity, and achieve goals.</p>
-            \(ActionButtons().render())
-        </div>
-    </section>
-    """
+        """
+        <section>
+            <div class="cta">
+                <h2 class="gradient-highlight">Get Started Now</h1>
+                <p>Sign up and get your life back on track! Stay organized, boost productivity, and achieve goals.</p>
+                \(ActionButtons().render())
+            </div>
+        </section>
+        """
     }
     
     func render() -> String {
-    """
-    \(heroSection())
-    \(featureList())
-    \(callToAction())
-    """
+        """
+        \(heroSection())
+        \(featureList())
+        \(callToAction())
+        """
     }
 }
