e538e551a93c50f03fa62e213f54ed2bacae1e20
Mac <maclong9@icloud.com>
2024-11-09 13:34:26 +0000

    chore: organise deps

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(
         .macOS(.v14),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.3.0"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.48.5"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0-rc.5"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-compression.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-fluent.git", from: "2.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.48.5"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.3.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
     ],
     targets: [
@@ -20,10 +20,10 @@ let package = Package(
             name: "App",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-                .product(name: "FluentKit", package: "fluent-kit"),
-                .product(name: "Hummingbird", package: "hummingbird"),
                 .product(name: "Bcrypt", package: "hummingbird-auth"),
+                .product(name: "FluentKit", package: "fluent-kit"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Hummingbird", package: "hummingbird"),
                 .product(name: "HummingbirdAuth", package: "hummingbird-auth"),
                 .product(name: "HummingbirdBasicAuth", package: "hummingbird-auth"),
                 .product(name: "HummingbirdCompression", package: "hummingbird-compression"),
@@ -37,8 +37,8 @@ let package = Package(
             name: "AppTests",
             dependencies: [
                 .byName(name: "App"),
-                .product(name: "HummingbirdTesting", package: "hummingbird"),
                 .product(name: "HummingbirdAuthTesting", package: "hummingbird-auth"),
+                .product(name: "HummingbirdTesting", package: "hummingbird"),
             ]
         ),
     ]
